d422d1125dc30ae6b8be3125824b43743076e8e9
nguyenvanan221 <85977454+nguyenvanan221@users.noreply.github.com>
2022-03-25 20:54:17 +0700

    Add files via upload

---
+++ DangNhap.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DemoQLTKQN
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_DN = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.txb_TK = new System.Windows.Forms.TextBox();
+            this.txb_MK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_DN
+            // 
+            this.btn_DN.Location = new System.Drawing.Point(82, 187);
+            this.btn_DN.Name = "btn_DN";
+            this.btn_DN.Size = new System.Drawing.Size(75, 23);
+            this.btn_DN.TabIndex = 0;
+            this.btn_DN.Text = "Đăng nhập";
+            this.btn_DN.UseVisualStyleBackColor = true;
+            this.btn_DN.Click += new System.EventHandler(this.btn_DN_Click);
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Location = new System.Drawing.Point(236, 187);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(75, 23);
+            this.btn_Thoat.TabIndex = 1;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // txb_TK
+            // 
+            this.txb_TK.Location = new System.Drawing.Point(176, 40);
+            this.txb_TK.Name = "txb_TK";
+            this.txb_TK.Size = new System.Drawing.Size(100, 20);
+            this.txb_TK.TabIndex = 2;
+            // 
+            // txb_MK
+            // 
+            this.txb_MK.Location = new System.Drawing.Point(176, 106);
+            this.txb_MK.Name = "txb_MK";
+            this.txb_MK.Size = new System.Drawing.Size(100, 20);
+            this.txb_MK.TabIndex = 3;
+            this.txb_MK.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tên Đăng nhập:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mật khẩu: ";
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txb_MK);
+            this.Controls.Add(this.txb_TK);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.btn_DN);
+            this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dang Nhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_DN;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.TextBox txb_TK;
+        private System.Windows.Forms.TextBox txb_MK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ DangNhap.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DemoQLTKQN
+{
+    public partial class DangNhap : Form
+    {
+        string chuoiketnoi = @"Data Source=ADMIN\SQLEXPRESS;Initial Catalog=QLTKNET;Integrated Security=True";
+        SqlConnection conn = null;
+        string tk1 = "admin";
+        string tk2 = "admin";
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_DN_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                conn = new SqlConnection(chuoiketnoi);
+                Form1 frm = new Form1();
+                conn.Open();
+                //string tk = txt_TK.Text;
+                //string mk = txt_MK.Text;
+                //string sql = "select TK,MK from TTTK where TK = '" + tk + "' and MK = '" + mk + "'";
+                //SqlCommand cmd = new SqlCommand(sql, conn);
+                //SqlDataReader dta = cmd.ExecuteReader();
+                //if (dta.Read() == true)
+                if(txb_TK.Text == tk1 && txb_MK.Text == tk2)
+                {
+                    MessageBox.Show("Đăng nhập thành công!","Thông báo");
+
+                    frm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Tên đăng nhập hoặc mật khẩu sai!","Thông báo");
+                }
+               
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi kết nối");
+            }
+
+            
+
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
